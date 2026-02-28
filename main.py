0b2d76709951d7b1900172dec0ce0e982ac367b5
Ganesh Godse <ganeshkgodse@gmail.com>
2026-02-28 15:06:34 +0530

    Dealt with format issues

--- main.py
+++ main.py
@@ -1,25 +1,37 @@
-
 import requests
 import time
 
 to_visit = ["https://news.ycombinator.com"]
 visited = {"https://news.ycombinator.com":True}
 
+# print(requests.get("https://trufflesecurity.com/blog/google-api-keys-werent-secrets-but-then-gemini-changed-the-rules").content.decode('utf-8'))
+nl = 2
+
 start = time.time()
 
-for i in range(10):
+for i in range(120):
 	url = to_visit[0]
-	print(url)
+	nl -=1
+	print(i+1,len(to_visit),nl,url)
 
 	response = requests.get(url)
-	data = response.content.decode('utf-8')
-	separated = data.split('href="')[1:]
+	if response.headers['Content-Type'] == "text/html; charset=utf-8":
+		data = response.content.decode('utf-8')
+	elif response.headers['Content-Type'] == "text/html":
+		data = response.content.decode('utf-8')
+	else:
+		visited[url] = True
+		to_visit = to_visit[1:]
+		continue
 
+	separated = data.split('href="')[1:]
+	# nl = 0
 	for part in separated:
 		link = part.split('"')[0]
-		if "http" in link and link not in visited:
-			to_visit.append(link)
-			# print(link)
+		if "http" in link:
+			nl += 1
+			if link not in visited:
+				to_visit.append(link)
 	visited[url] = True
 	to_visit = to_visit[1:]
 
